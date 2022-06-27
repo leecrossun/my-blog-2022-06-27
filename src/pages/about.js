23364124d4e9b8c1a20fb41782db5a047462c1e1
hyosunLee <leecrossun@gmail.com>
2022-06-27 16:13:11 +0900

    add aboutPage

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <main>
+      <title>자기소개</title>
+      <h1>자기소개 페이지 입니다.</h1>
+    </main>
+  );
+}
+
+export default AboutPage;
