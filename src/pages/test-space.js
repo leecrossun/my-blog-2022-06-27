b5bc12f56491d02286aaa63055821caf9ff18b05
hyosunLee <leecrossun@gmail.com>
2022-06-27 16:14:38 +0900

    add testPages

---
+++ src/pages/test-space.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function TestSpacePage() {
+  return (
+    <main>
+      <title>테스트 스페이스</title>
+      <h1>테스트 스페이스 페이지 입니다.</h1>
+    </main>
+  );
+}
+
+export default TestSpacePage;
---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function TestPage() {
+  return (
+    <main>
+      <title>테스트</title>
+      <h1>테스트 페이지 입니다.</h1>
+    </main>
+  );
+}
+
+export default TestPage;
