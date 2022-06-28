08648ca108fa2db4b12f557555ec716936756e62
hyosunLee <leecrossun@gmail.com>
2022-06-28 18:11:06 +0900

    Add new post, Update index page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 function AboutPage() {
   return (
     <Layout pageTitle="About Me">
       <p>
+      <StaticImage
+        alt="Main Page"
+        src="https://picsum.photos/id/1013/500/300"
+      />
         Hi I'm Back-end Developer hyosun Lee. <br/>
         Welcome to my blog
       </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,7 @@ import Layout from "../components/layout";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <StaticImage
-        alt="Main Page"
-        src="https://picsum.photos/id/1013/500/300"
-      />
+      <img src={'https://mblogthumb-phinf.pstatic.net/MjAyMDA3MjBfMjM2/MDAxNTk1MTk1MTg2MTEz.5YYUfm9wE0IZ_ErObyab4YFQ3PLZz_UDFRpv1kY7s7Yg.suv2K4NSn2R-NCIYGBjERF5lROx_JFHfYlxBj8g2Yrog.GIF.lhj08152002/1595195184174.GIF?type=w800'} alt="main" />
       <p>This is developer blog.</p>
     </Layout>
   );
