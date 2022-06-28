b2aa3eb3f8661f66a9ac8fb4ce5af8f7020afcfa
hyosunLee <leecrossun@gmail.com>
2022-06-28 14:58:03 +0900

    Add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://picsum.photos/id/1013/500/300"
+      />
       <p>This is developer blog.</p>
     </Layout>
   );
